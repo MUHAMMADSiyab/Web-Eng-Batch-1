c7cf79660c0d5688a1c2901966b4f2e14e371dc0
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-01-10 22:18:08 +0500

    JavaScript directory added

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+// Output using `console.log` method
+console.log("Hello World");
+
+document.write("Hello World"); // Not recommended
+
+// window.alert("Hello World");
+
+// Camel Case
+document.getElementById("myheading").innerText = "Welcome to the Web";
